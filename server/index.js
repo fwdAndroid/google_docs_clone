9e61d2e9fda4aaebcd25025f98c22e579f6b20c0
fwdAndroid <fwdkaleem@gmail.com>
2022-10-03 18:01:30 +0500

    MongoDB Setup

--- server/index.js
+++ server/index.js
@@ -4,6 +4,20 @@ const mongoose = require("mongoose");
 const PORT = process.env.PORT | 3001;
 
 const  app = express();
+
+const DB =
+  "mongodb+srv://fwdkaleem:Zeo123456@cluster0.pimqn8j.mongodb.net/?retryWrites=true&w=majority";
+
+  mongoose
+  .connect(DB)
+  .then(() => {
+    console.log("Connection successful!");
+  }).catch((err) => {
+    console.log(err);
+  })
+
+
+
 app.listen(PORT, "0.0.0.0", () => {
     console.log(`connected at port ${PORT}`);
   });
